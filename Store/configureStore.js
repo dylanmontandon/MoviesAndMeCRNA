3661263f756af7fe682f44d373be3ff8b2386568
Dylan Montandon <dylan.montandon@he-arc.ch>
2019-10-07 11:10:00 +0200

    add persistance to the app

--- Store/configureStore.js
+++ Store/configureStore.js
@@ -1,7 +1,15 @@
 // Store/configureStore.js
 
-import { createStore, combineReducers } from 'redux';
+import { createStore } from 'redux'
 import toggleFavorite from './Reducers/favoriteReducer'
 import setAvatar from './Reducers/avatarReducer'
+import { persistCombineReducers } from 'redux-persist'
+import asyncStorage from '@react-native-community/async-storage';
 
-export default createStore(combineReducers({toggleFavorite, setAvatar}))
+//permet de persister les reducers
+const rootPersistConfig = {
+  key: 'root',
+  storage: asyncStorage
+}
+
+export default createStore(persistCombineReducers(rootPersistConfig, {toggleFavorite, setAvatar}))
